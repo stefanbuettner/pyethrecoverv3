bcd9fa82c6db28e9913a764de42836627a381fa0
Stefan Büttner <buettner.stefan@web.de>
2018-01-08 03:43:08 +0100

    Add pw generator and port original pyethrecover cli functionality.

---
+++ pwgen.py
@@ -0,0 +1,55 @@
+class PwGenerator(object):
+    def __init__(self, alphabet, max_length=-1, min_length=1):
+        self.alphabet = alphabet
+        self.i = 0
+        n = len(alphabet)
+        while min_length > 0:
+            self.i = 1 + self.i * n
+            min_length -= 1
+        self.max_length = 0
+        while max_length >= 0:
+            self.max_length = 1 + self.max_length * n
+            max_length -= 1
+        self.max_length -= 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        s = self._get_str_(self.i)
+        self.i += 1
+        return s
+
+    def _get_str_(self, i):
+        if self.max_length >= 0 and i > self.max_length:
+            raise StopIteration()
+        s = ""
+        m = len(self.alphabet)
+        while i > 0:
+            s = self.alphabet[(i - 1) % m] + s
+            i = (i-1) // m
+        return s
+
+    def to_json(self):
+        import json
+        return json.dumps(self, default=lambda o : o.__dict__, sort_keys=True, indent=4)
+
+    def from_json(self):
+        raise NotImplemented()
+
+
+digits = "0123456789"
+alpha = "abcdefghijklmnopqrstuvwxyz"
+Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+symbols = "@&$?!"
+
+if __name__ == "__main__":
+    import json
+    gen = PwGenerator(digits + alpha + Alpha + symbols, min_length=8)
+    with open('pwgenerator.json', "w") as file:
+        file.write(gen.to_json())
+    for pw in gen:
+        print(pw)
--- pyethrecover_v3.py
+++ pyethrecover_v3.py
@@ -7,49 +7,24 @@ import sys
 import traceback
 from joblib import Parallel, delayed
 import multiprocessing
+import pwgen
+import signal, os
 
-wordlist = "words3.txt"
-with open(wordlist) as f:
-    dictlist = f.read().splitlines()
-
-w = {
-  "address": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  "crypto": {
-    "cipher": "aes-128-ctr",
-    "ciphertext": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    "cipherparams": {
-      "iv": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-    },
-    "kdf": "scrypt",
-    "kdfparams": {
-      "dklen": 32,
-      "n": 262144,
-      "p": 1,
-      "r": 8,
-      "salt": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-    },
-    "mac": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  },
-  "id": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  "version": 3
-}
-
-# Constructing passwords from possible combinations (see doc of pyethrecover)
-grammar=[
-    ('abe ', 'dan ', 'mary '),
-    ('lincoln ', 'jesus ', 'johnson '),
-    dictlist  # we append the wordlist. This isn't necessary if you think you've included all possible words above
-]
 
 # print(grammar)
 def generate_all(el, tr): #taken from pyethrecover
     if el:
-        for j in xrange(len(el[0])):
+        for j in range(len(el[0])):
             for w in generate_all(el[1:], tr + el[0][j]):
                 # print(w)
                 yield w
     else:
         yield tr
+
+
+class PasswordFoundException(Exception):
+    pass
+
 
 def attempt(w, pw):
     # print(pw)
@@ -64,7 +39,7 @@ def attempt(w, pw):
     if len(pw) < 10:
         return ""
     try:
-        o = decode_keystore_json(w,pw)
+        o = decode_keystore_json(w, pw)
         print(o)
         # print (pw)q
         raise PasswordFoundException(
@@ -72,6 +47,7 @@ def attempt(w, pw):
     except ValueError as e:
         # print(e)
         return ""
+
 
 class Counter(object):
     def __init__(self):
@@ -85,11 +61,74 @@ class Counter(object):
     def value(self):
         return self.val.value
 
+
+pwgenerator = pwgen.PwGenerator(pwgen.digits + pwgen.alpha + pwgen.Alpha + pwgen.symbols, min_length=8)
+
+
+def interrupt_handler(signum, frame):
+    with open("pwgenerator.json", "w") as file:
+        file.write(pwgenerator.to_json())
+    exit(1)
+
+
 def __main__():
+
+    signal.signal(signal.SIGINT, interrupt_handler)
+
+    # Option parsing
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('-p', '--password',
+                      default=None, dest='pw',
+                      help="A single password to try against the wallet.")
+    parser.add_option('-f', '--passwords-file',
+                      default=None, dest='pwfile',
+                      help="A file containing a newline-delimited list of passwords to try. (default: %default)")
+    parser.add_option('-s', '--password-spec-file',
+                      default=None, dest='pwsfile',
+                      help="A file containing a password specification")
+    parser.add_option('-w', '--wallet',
+                      default='wallet.json', dest='wallet',
+                      help="The wallet against which to try the passwords. (default: %default)")
+
+    (options, args) = parser.parse_args()
+
+    try:
+        with open(options.wallet) as wallet_file:
+            try:
+                w = json.load(wallet_file)
+            except json.JSONDecodeError:
+                print("Corrupt file '%s'." % options.wallet, file=sys.stderr)
+                exit(1)
+    except IOError:
+        print("Could not load file '%s'." % options.wallet, file=sys.stderr)
+        exit(1)
+
+    if not w:
+        print("Wallet file not found! (-h for help)")
+        exit(1)
+
+    pwds = []
+
+    if options.pw:
+        pwds.append(options.pw)
+
+    if options.pwfile:
+        try:
+            with open(options.pwfile) as pwfile:
+                pwlist = pwfile.read().splitlines()
+                pwds.extend(pwlist)
+        except:
+            print("Password file not found! (-h for help)")
+            exit(1)
+
+    if options.pwsfile:
+        grammar = eval(open(options.pwsfile, 'r').read())
+        pwds = itertools.chain(pwds, generate_all(grammar, ''))
+
     global counter
     counter = Counter()
-    pwds = []
-    pwds = itertools.chain(pwds, generate_all(grammar,''))
+    pwds = itertools.chain(pwds, pwgenerator)
 
     # n_pws = len(list(pwds))
     # print 'Number of passwords to test: %d' % (n_pws)
@@ -97,11 +136,13 @@ def __main__():
     try:
         Parallel(n_jobs=-1)(delayed(attempt)(w, pw) for pw in pwds)
         # print("\n")
-    except Exception, e:
+    except Exception as e:
         traceback.print_exc()
         while True:
             sys.stdout.write('\a')
             sys.stdout.flush()
+    exit(0)
+
 
 if __name__ == "__main__":
     __main__()
