5a195f28015ca1ebe7bff5667b34eae92b30791e
Stefan Büttner <buettner.stefan@web.de>
2018-02-08 14:30:23 +0100

    First working version of BFS.

--- pwgen.py
+++ pwgen.py
@@ -59,7 +59,7 @@ class Rule:
         if len(match) != 1:
             raise RuntimeError("match must be one character but is %s" % match)
         self.match = match
-        self.replacements = match + replacements
+        self.replacements = replacements
         self.s = ""
         self.i = 0
         self.matches = []
@@ -132,7 +132,9 @@ class RuleCollection:
         self.rules = rules
         self.replacements = dict()
         self.replacement_indices = list()
-        self.beginning = True
+        self.replacement_count = 0
+        self.match_combinations = None
+        self.replacement_combinations = None
         if self.string != "":
             self.reset(string)
 
@@ -147,67 +149,57 @@ class RuleCollection:
                 if mIdx in self.replacements:
                     self.replacements[mIdx].extend(r.replacements)
                 else:
+                    self.replacement_indices.append(mIdx)
                     self.replacements[mIdx] = r.replacements
-        if len(self.replacements) > 0:
-            self.replacement_indices.append((self._find_next_free_match_(0), 0))
-            self.beginning = True
+        self.match_combinations = itertools.combinations(self.replacement_indices, self.replacement_count)
+        self.replacement_combinations = itertools.product()
 
     def next(self):
         return self.__next__()
 
     def __next__(self):
-        if len(self.replacement_indices) <= 0:
-            raise StopIteration
+        try:
+            combination = self.match_combinations.__next__()
 
-        s = self.string
-        offset = 0
-        print(self.replacement_indices)
-        for match_idx, rep_idx in self.replacement_indices:
-            s = s[:match_idx + offset] + self.replacements[match_idx][rep_idx] + s[match_idx + offset + 1:]
+        except StopIteration:
+            self.replacement_count += 1
+            self.match_combinations = itertools.combinations(self.replacement_indices, self.replacement_count)
+            combination = self.match_combinations.__next__()
+        print(combination)
 
-        must_pop = True
-        while must_pop:
-            must_pop = False
-            pos, k = self.replacement_indices.pop()
-            k += 1
-            if k >= len(self.replacements[pos]):
-                new_pos = self._find_next_free_match_(pos + 1)
-                if new_pos >= len(self.string):
-                    new_pos = self._find_next_free_match_(0)
-                    if new_pos != pos and new_pos < len(self.string):
-                        self.replacement_indices.append((new_pos, 1))
-                        # new_pos = self._find_next_free_match_(new_pos + 1)
-                        # if new_pos < len(self.string):
-                        #     self.replacement_indices.append((new_pos, 1))
-                    else:
-                        must_pop = True
-                else:
-                    self.replacement_indices.append((new_pos, 1))
+        # s = self.string
+        # offset = 0
+        # print(self.replacement_indices)
+        # for match_idx, rep_idx in self.replacement_indices:
+        #     s = s[:match_idx + offset] + self.replacements[match_idx][rep_idx] + s[match_idx + offset + 1:]
+        return self.string
+
+
+def combine_rules(string, rules):
+    replacements = dict()
+    replacement_indices = list()
+
+    for r in rules:
+        r.reset(string)
+        for mIdx in r.find_matches():
+            if mIdx in replacements:
+                replacements[mIdx].extend(r.replacements)
             else:
-                self.replacement_indices.append((pos, k))
-        return s
+                replacement_indices.append(mIdx)
+                replacements[mIdx] = r.replacements
 
-    def _find_next_free_match_(self, pos):
-        """
-        Finds the next free match beginning at position pos.
+    for replacement_count in range(len(replacement_indices) + 1):
+        for match_comb in itertools.combinations(replacement_indices, replacement_count):
+            rep = list()
+            for m in match_comb:
+                rep.append(replacements[m])
 
-        :return: The index of the next free match.
-        """
-        #in_replacements = True
-        #while in_replacements:
-        for m in self.replacements:
-            if m < pos:
-                continue
-            in_replacements = False
-            for p, l in self.replacement_indices:
-                if p == m:
-                    in_replacements = True
-                    break
-            if not in_replacements:
-                return m
-        return len(self.string)
-
-
+            for rep_comb in itertools.product(*rep):
+                #print(str(match_comb) + " " + str(rep_comb))
+                s = string
+                for i in range(len(match_comb)):
+                    s = s[:match_comb[i]] + rep_comb[i] + s[match_comb[i] + 1:]
+                yield s
 
 
 
@@ -244,6 +236,6 @@ if __name__ == "__main__":
 
     r_p = Rule("p", "P", r_t)
 
-    pwds = RuleCollection([r_A, r_e], "AnAbel")
+    pwds = combine_rules("AnAbel", [r_A, r_e])
     for pw in pwds:
         print(pw)
